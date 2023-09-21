eaf8e2f36eca1dcdbcbe8864fd5f3aa92b400580
StefanoRucci <stefano.rucci981@gmail.com>
2023-09-21 22:24:18 +0200

    little updates

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 import gym
 import time
+import numpy as np
+import random
+from IPython.display import clear_output
+
+# setup the environment
 env=gym.make("CliffWalking-v0",render_mode='human')
 env.reset()
 env.render()
 print('Initial state of the system')
+
 # observation space - states of the environment (they are 48) 
 print("Observation space", env.observation_space)
 
@@ -15,18 +21,22 @@ numberOfIterations=30
 reward=0
 
 for i in range(numberOfIterations):
-    randomAction= env.action_space.sample()
+    randomAction= env.action_space.sample() #This is a randomaction
     #print("Random action: ", randomAction)
-    returnValue=env.step(randomAction)
+    returnValue=env.step(randomAction) #The return state of the random action
     #print("ReturnValue: ", returnValue)
     env.render()
     print('Iteration: {} and action {}'.format(i+1,randomAction))
     reward+=returnValue[1]
     time.sleep(2)
+
+    # if fall in the climb
     if returnValue[1]==-100:
         print("The reward is: ",reward)
         reward=0
         env.reset()
+
+    # if goal
     if returnValue[0] == 47:
         print("The reward is: ",reward)
         reward=0
