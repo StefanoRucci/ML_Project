894c3a5ddb1c607cef852e7b9f6766d84541449b
StefanoRucci <stefano.rucci981@gmail.com>
2023-09-21 22:08:54 +0200

    little updates

--- src/main.py
+++ src/main.py
@@ -12,15 +12,27 @@ print("Action space", env.action_space)
 
 numberOfIterations=30
 
+reward=0
+
 for i in range(numberOfIterations):
     randomAction= env.action_space.sample()
-    print("Random action: ", randomAction)
+    #print("Random action: ", randomAction)
     returnValue=env.step(randomAction)
-    print("ReturnValue: ", returnValue)
+    #print("ReturnValue: ", returnValue)
     env.render()
     print('Iteration: {} and action {}'.format(i+1,randomAction))
+    reward+=returnValue[1]
     time.sleep(2)
     if returnValue[1]==-100:
+        print("The reward is: ",reward)
+        reward=0
+        env.reset()
+    if returnValue[0] == 47:
+        print("The reward is: ",reward)
+        reward=0
         env.reset()
 
+    print("##########################################")
+
+print("The reward is: ",reward)
 env.close()
